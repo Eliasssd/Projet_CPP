8601c2477a68591867d9e627de215b99c81fcf88
toniokan <113203322+toniokan@users.noreply.github.com>
2023-12-19 18:16:47 +0100

    POst request support

--- servercrow.cpp
+++ servercrow.cpp
@@ -1,10 +1,36 @@
-#include <mysql_driver.h>
-#include <mysql_connection.h>
-#include <cppconn/resultset.h>
-#include <cppconn/statement.h>
-#include <cppconn/prepared_statement.h>
 #include <iostream>
 #include "crow.h"
+#include "/usr/include/sqlite3.h"
+#include <string>
+#include <sstream>
+#include <iomanip>
+#include "jsoncpp/json/json.h"
+
+static int callback(void *NotUsed, int argc, char **argv, char **szColName)
+{
+  for(int i = 0; i < argc; i++)
+  {
+    std::cout << szColName[i] << " = " << argv[i] << std::endl;
+  }
+
+  std::cout << "\n";
+
+  return 0;
+
+}
+
+std::string hexToStr(const std::string& hex) {
+    std::string str;
+    str.reserve(hex.length() / 2);
+
+    for (size_t i = 0; i < hex.length(); i += 2) {
+        std::string byteString = hex.substr(i, 2);
+        char byte = static_cast<char>(std::stoul(byteString, nullptr, 16));
+        str.push_back(byte);
+    }
+
+    return str;
+}
 
 int main() {
 
@@ -12,31 +38,81 @@ int main() {
 
     const int port=18080;
 
-    try {
-        sql::mysql::MySQL_Driver* driver = sql::mysql::get_driver_instance();//instance du pilote mysql pour demander la connexion
-        std::unique_ptr<sql::Connection> con(driver->connect("tcp://127.0.0.1:3306", "serveruser", "serveruserpwd"));//établit la connexion avec la base de données
-        con->setSchema("test_db");//selection de la base de données
+    sqlite3 *db;
+    char *szErrMsg = 0;
 
-        std::unique_ptr<sql::Statement> stmt(con->createStatement());//création de l'objet statement nécessaire pour faire des requetes la base
-        std::unique_ptr<sql::ResultSet> res(stmt->executeQuery("SELECT * FROM users"));//exécution d'une requete
+    // open database
+    int rc = sqlite3_open("Sqlite_Test.db", &db);
 
-        while (res->next()) {//parcours des lignes retournées par la requete 
-            std::cout << res->getString("your_column") << std::endl;
-        }
-    } catch (sql::SQLException &e) {//gestion d'erreur
-        std::cerr << "Error: " << e.what() << std::endl;
-        return 1;
+    if(rc)
+    {
+        std::cout << "Can't open database\n";
+    } else {
+        std::cout << "Open database successfully\n";
     }
 
-    CROW_ROUTE(app, "/api/")//exemple de route pour la mise en place du REST API
+    // prepare our sql statements
+    const char *pSQL[6];
+    pSQL[0] = "CREATE TABLE Employee(Firstname varchar(30), Lastname varchar(30), Age smallint)";
+    pSQL[1] = "INSERT INTO Employee(Firstname, Lastname, Age) VALUES ('Woody', 'Alan', 45)";
+    pSQL[2] = "INSERT INTO Employee(Firstname, Lastname, Age) VALUES ('Micheal', 'Bay', 38)";
+    pSQL[3] = "SELECT * FROM Employee";
+
+    // execute sql
+    for(int i = 0; i < 4; i++)
+    {
+        rc = sqlite3_exec(db, pSQL[i], callback, 0, &szErrMsg);
+        if(rc != SQLITE_OK)
+        {
+        std::cout << "SQL Error: " << szErrMsg << std::endl;
+        sqlite3_free(szErrMsg);
+        break;
+        }
+    }
+
+    // close database
+    if(db)
+    {
+        sqlite3_close(db);
+    }
+
+    CROW_ROUTE(app, "/api/db")//exemple de route pour la mise en place du REST API
     ([](){
-        return "Hello, world!";
+        //return "Hello, world!";
+        return "{a:3,b:3}";
     });
 
+    CROW_ROUTE(app, "/api/db2")//exemple de route pour la mise en place du REST API
+    ([](){
+        //return "Hello, world!";
+        return "{a:3,b:4}";
+    });
+
+    CROW_ROUTE(app, "/api/post").methods(crow::HTTPMethod::Post)([](const crow::request& req){
+        crow::json::rvalue x = crow::json::load(req.body);//charge le corps de la requete post et on teste si elle est convenable
+        if (!x)
+            return crow::response(400, "Invalid JSON");//sinon on envoie un code d'erreur HTTP
+        
+        //std::cout<<req.body<<std::endl;//afficher la body (debogage)
+
+        Json::Value obj;
+        std::stringstream(req.body) >> obj;//on met req.body en objet JSON lisible par C++
+
+
+        std::string value1 = obj["param1"].asString();//Lire une valeur et la mettre dans une variable pour l'utiliser
+        std::cout<<"Value : "<<value1<<std::endl;
+        
+        // Create a response
+        std::ostringstream os;
+        os << "message de reponse" ;
+        return crow::response(200,os.str());//on envoie un code de retour positif
+    });
+
+
     std::cout<<"Listening on port : "<<port<<std::endl;
-    app.port(port).multithreaded().run();//server à l'écoute en gestion de requete multithreading
+    app.port(port).multithreaded().run();//serveur à l'écoute en gestion de requete multithreading
 
     return 0;
 }
 
-//g++ -o crowserver crowserver.cpp -I/home/user18/ProjetCPP/crow/include -lmysqlcppconn -lboost_system -lpthread
+//g++ -o cs crowserver.cpp  -I/home/user18/ProjetCPP/crow/include -lsqlite3 -lboost_system -lpthread -ljsoncpp  //Celle-ci
