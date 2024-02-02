2e733a67b17447799a282575555e423784245b6d
toniokan <113203322+toniokan@users.noreply.github.com>
2024-02-02 01:23:37 +0100

    Update datetime.cpp

--- datetime.cpp
+++ datetime.cpp
@@ -6,25 +6,14 @@
 #include <string>
 #include <ctime>
 
-const std::string getnowtime(const int offset)
+const std::string getnowtime(const int offset)//Retourne la timestamp actuelle, on peut rajouter offset minutes
 {
-    auto now = std::chrono::system_clock::now()+ std::chrono::minutes(offset);
+    auto now = std::chrono::system_clock::now()+std::chrono::minutes(offset);
     std::time_t now_time_t = std::chrono::system_clock::to_time_t(now);
     std::tm* now_tm = std::localtime(&now_time_t);
     const int TIME_BUF_SIZE = 80;
     char time_buffer[TIME_BUF_SIZE];
     std::strftime(time_buffer, TIME_BUF_SIZE, "%Y:%m:%d %H:%M:%S", now_tm);
     std::string timestamp(time_buffer);
-    //std::cout << "Current time: " << timestamp << std::endl;
     return timestamp;
 }
-
-/*
-int main() {
-    
-    std::string nowtime=getnowtime();
-
-    std::cout<<nowtime<<std::endl;
-
-    return 0;
-}*/
